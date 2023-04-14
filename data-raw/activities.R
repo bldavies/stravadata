e1e277aa5635cf4154533b40ab5390d5e9a36cf9
Ben Davies <git@bldavies.fastmail.com>
2023-04-13 20:48:36 -0700

    Cache activities by month

--- data-raw/activities.R
+++ data-raw/activities.R
@@ -8,56 +8,100 @@
 
 # Initialization ----
 
+# Load packages
 library(dplyr)
 library(lubridate)
 library(jsonlite)
 library(readr)
+library(usethis)
 
+# Set input directory
+base_dir = 'data-raw/downloads'
 
-# Data import ----
-
-cache_files = list.files('data-raw/downloads', 'details[.]json', full.names = T, recursive = T)
-
-cache_list = lapply(cache_files, read_json)
-
-
-# Data export ----
-
+# Define function for converting NULLs to NAs
 null2na = function(x) ifelse(!is.null(x), x, NA)
 
-activities = cache_list %>%
-  lapply(
-    function(x) {
-      tibble(
-        id           = x$id,
-        name         = x$name,
-        type         = x$type,
-        workout_type = null2na(x$workout_type),
-        indoor       = x$trainer,
-        commute      = x$commute,
-        private      = x$private,
-        start_time   = as_datetime(x$start_date_local),
-        timezone     = x$timezone,
-        distance     = x$distance,
-        time_moving  = x$moving_time,
-        time_total   = x$elapsed_time,
-        elev_gain    = x$total_elevation_gain,
-        mean_hr      = null2na(x$average_heartrate),
-        mean_cadence = null2na(x$average_cadence),
-        mean_temp    = null2na(x$average_temp),
-        exertion     = null2na(x$perceived_exertion),
-        suffer_score = null2na(x$suffer_score)
-      )
+
+# Caching ----
+
+# Initialize cache directory
+cache_dir = 'data-raw/activities'
+if (!dir.exists(cache_dir)) dir.create(cache_dir)
+
+# Iterate over years
+year_dirs = list.dirs(base_dir, recursive = F)
+for (year_dir in year_dirs) {
+  
+  # Iterate over months
+  month_dirs = list.dirs(year_dir, recursive = F)
+  for (month_dir in month_dirs) {
+    
+    # Initialize cache file
+    cache_file = sub(paste0(base_dir, '/(.*)/(.*)'), paste0(cache_dir, '/\\1-\\2.csv'), month_dir)
+    
+    # List detail files
+    detail_files = list.files(month_dir, 'details[.]json', full.names = T, recursive = T)
+    
+    # Create/update cache
+    if (!file.exists(cache_file) | file.mtime(cache_file) < max(file.mtime(detail_files))) {
+      
+      detail_files %>%
+        lapply(read_json) %>%
+        lapply(
+          function(x) {
+            tibble(
+              id           = x$id,
+              name         = x$name,
+              type         = x$type,
+              workout_type = null2na(x$workout_type),
+              indoor       = x$trainer,
+              commute      = x$commute,
+              private      = x$private,
+              start_time   = as_datetime(x$start_date_local),
+              timezone     = x$timezone,
+              distance     = x$distance,
+              time_moving  = x$moving_time,
+              time_total   = x$elapsed_time,
+              elev_gain    = x$total_elevation_gain,
+              mean_hr      = null2na(x$average_heartrate),
+              mean_cadence = null2na(x$average_cadence),
+              mean_temp    = null2na(x$average_temp),
+              exertion     = null2na(x$perceived_exertion),
+              suffer_score = null2na(x$suffer_score)
+            )
+          }
+        ) %>%
+        bind_rows() %>%
+        write_csv(cache_file)
+      
     }
-  ) %>%
-  bind_rows() %>%
-  mutate(workout_type = replace(workout_type, workout_type %in% c(0, 10), NA)) %>%
-  arrange(id)
+  }
+}
 
-write_csv(activities, 'data-raw/activities.csv')
 
-if (!dir.exists('data')) dir.create('data/')
-save(activities, file = 'data/activities.rda', version = 2, compress = 'bzip2')
+# Collation ----
+
+# Initialize output file
+out_file = 'data-raw/activities.csv'
+
+# List files
+cache_files = list.files(cache_dir, '[.]csv', full.names = T)
+
+# Create/update table
+if (!file.exists(out_file) | file.mtime(out_file) < max(file.mtime(cache_files))) {
+  
+  # Build table
+  activities = cache_files %>%
+    lapply(read_csv, show_col_types = F) %>%
+    bind_rows() %>%
+    mutate(workout_type = replace(workout_type, workout_type %in% c(0, 10), NA)) %>%
+    arrange(id)
+  
+  # Export table
+  write_csv(activities, out_file)
+  use_data(activities, overwrite = T)
+  
+}
 
 
 # Session info ----
