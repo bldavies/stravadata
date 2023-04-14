1b81311616e0f032e990e7007df7839616196020
Ben Davies <git@bldavies.fastmail.com>
2023-04-13 21:33:33 -0700

    Cache best efforts by month

--- data-raw/best-efforts.R
+++ data-raw/best-efforts.R
@@ -6,48 +6,86 @@
 # April 2023
 
 
-if (length(dir('data-raw/downloads')) == 0) {
-  stop('data-raw/downloads/ must not be empty.')
-}
-
-if (file_test('-ot', 'data/activities.rda', 'data/best-efforts.rda')) {
-  stop('Data already up to date.')
-}
-
-
 # Initialization ----
 
+# Load packages
 library(dplyr)
 library(jsonlite)
 library(readr)
+library(usethis)
+
+# Set input directory
+base_dir = 'data-raw/downloads'
 
 
-# Data extraction and export ----
+# Caching ----
 
-cache_files = list.files('data-raw/downloads', 'details[.]json', full.names = T, recursive = T)
+# Initialize cache directory
+cache_dir = 'data-raw/best-efforts'
+if (!dir.exists(cache_dir)) dir.create(cache_dir)
 
-cache_list = lapply(cache_files, read_json)
-
-best_efforts = cache_list %>%
-  lapply(function(x) x$best_efforts) %>%
-  unlist(recursive = F) %>%
-  lapply(
-    function(x) {
-      tibble(
-        id = x$activity$id,
-        effort = x$name,
-        start_index = x$start_index,
-        end_index = x$end_index
-      )
+# Iterate over years
+year_dirs = list.dirs(base_dir, recursive = F)
+for (year_dir in year_dirs) {
+  
+  # Iterate over months
+  month_dirs = list.dirs(year_dir, recursive = F)
+  for (month_dir in month_dirs) {
+    
+    # Initialize cache file
+    cache_file = sub(paste0(base_dir, '/(.*)/(.*)'), paste0(cache_dir, '/\\1-\\2.csv'), month_dir)
+    
+    # List detail files
+    detail_files = list.files(month_dir, 'details[.]json', full.names = T, recursive = T)
+    
+    # Create/update cache
+    if (!file.exists(cache_file) | file.mtime(cache_file) < max(file.mtime(detail_files))) {
+      
+      detail_files %>%
+        lapply(read_json) %>%
+        lapply(function(x) x$best_efforts) %>%
+        unlist(recursive = F) %>%
+        lapply(
+          function(x) {
+            tibble(
+              id = x$activity$id,
+              effort = x$name,
+              start_index = x$start_index,
+              end_index = x$end_index
+            )
+          }
+        ) %>%
+        bind_rows() %>%
+        mutate_at(c('start_index', 'end_index'), function(x) as.integer(x + 1)) %>%
+        write_csv(cache_file)
+      
     }
-  ) %>%
-  bind_rows() %>%
-  mutate_at(c('start_index', 'end_index'), function(x) as.integer(x + 1)) %>%
-  arrange(id, effort)
+  }
+}
 
-write_csv(best_efforts, 'data-raw/best-efforts.csv')
 
-save(best_efforts, file = 'data/best-efforts.rda', version = 2, compress = 'bzip2')
+# Collation ----
+
+# Initialize output file
+out_file = 'data-raw/best-efforts.csv'
+
+# List files
+cache_files = list.files(cache_dir, '[.]csv', full.names = T)
+
+# Create/update table
+if (!file.exists(out_file) | file.mtime(out_file) < max(file.mtime(cache_files))) {
+  
+  # Build table
+  best_efforts = cache_files %>%
+    lapply(read_csv, show_col_types = F) %>%
+    bind_rows() %>%
+    arrange(id, effort)
+  
+  # Export table
+  write_csv(best_efforts, out_file)
+  use_data(best_efforts, overwrite = T)
+  
+}
 
 
 # Session info ----
